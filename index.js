c7be4b6def5da14e7debc91516530f71314a65d4
Dyphnah <75563233+Dyphnah@users.noreply.github.com>
2023-04-28 16:13:18 +0300

    Add files via upload
    Updated JS

--- index.js
+++ index.js
@@ -11,3 +11,33 @@
         document.body.classList.toggle("light-mode");
     })
 })();
+
+// Add submit event listener to form
+
+const form = document.querySelector('.contact-form');
+form.addEventListener('submit', (e) => {
+  e.preventDefault(); 
+
+  const name = document.querySelector('#myName').value;
+  const email = document.querySelector('#myEmail').value;
+  const subject = document.querySelector('#subject').value;
+  const message = document.querySelector('#myMessage').value;
+  const emailMessage = `Name: ${name}\nEmail: ${email}\nSubject: ${subject}\nMessage: ${message}`;
+
+  // Send email using SMTP.js library. I had to google this. not sure it is working.
+  Email.send({
+    Host: 'smtp.gmail.com',
+    Username: 'YOUR_GMAIL_ADDRESS',
+    Password: 'YOUR_GMAIL_PASSWORD',
+    To: 'nyamongodyphnah4@gmail.com',
+    From: 'YOUR_GMAIL_ADDRESS',
+    Subject: subject,
+    Body: emailMessage,
+  }).then((message) => {
+    form.reset();
+    alert('Your message has been sent successfully!');
+  });
+});
+
+
+ 
